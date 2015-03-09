5298822096bee110e820ef65fc00a031ee2534fe
Joseba Legarreta <josebalega@gmail.com>
2015-03-09 16:43:10 +0100

    Download song by name and artist

---
+++ machines/download-song-by-name-and-artist.js
@@ -0,0 +1,96 @@
+module.exports = {
+
+
+  friendlyName: 'Download song by name and artist',
+
+
+  description: 'Download a song, and save it into a folder, by name and artist',
+
+
+  extendedDescription: '',
+
+
+  inputs: {
+
+    name: {
+      example: 'Bohemian Rhapsody',
+      description: 'Song name, not case sensitive',
+      required: true
+    },
+
+    artist: {
+      example: 'Queen',
+      description: 'Artist name, not case sensitive',
+      required: true
+    },
+
+    path: {
+      example: '/userId/coolFolder',
+      description: 'ABSOLUTE Path to the folder where the song will be saved, if the folder doesn\'t exist it will be created',
+      required: true
+    }
+
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred.',
+    },
+
+    notFound: {
+      description: 'Song not found, try with another name/artist'
+    },
+
+    downloadLimitExceded: {
+      description: 'It looks like we are banned by Grooveshark, please try again in a few hours'
+    },
+
+    success: {
+      description: 'Downloaded.',
+    }
+
+  },
+
+
+  fn: function (inputs, exits) {
+
+    var GS = require('grooveshark-downloader');
+    var async = require('async');
+    var fs = require('fs');
+    var request = require('request');
+
+    GS.Tinysong.getSongInfo(inputs.name, inputs.artist, function(err, info) {
+      if(err) return exits.error(err);
+
+      GS.Grooveshark.getStreamingUrl(info.SongID, function(err, streamUrl) {
+        if(err) return exits.error(err);
+
+        if(!fs.existsSync(inputs.path)){
+          var mkdirp = require('mkdirp');
+          mkdirp.sync(inputs.path);
+        }
+
+        var songPath = inputs.path + '/' + info.SongName + '.mp3';
+
+        request
+          .get(streamUrl)
+           .on('error', function(err){
+             return exits.error(err);
+           })
+           .on('end', function(){
+             return exits.success();
+           })
+           .pipe(fs.createWriteStream(songPath));
+      });
+    });
+
+  },
+
+
+
+};
